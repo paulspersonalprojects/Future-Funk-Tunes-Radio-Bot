97a60ff3f87eb68cc3b2028eeddc2600a64802f5
Paul Nguyen <69123371+IdioticFruit@users.noreply.github.com>
2025-01-20 18:46:16 -0700

    - Added ffmpeg to stream URL - Used Discord.py Package to run Slash Commands - Fixed Connect to include auto move between vcs - fixed Disconnect and ping - removed .env secret

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
-import discord
-from discord.ext import commands
+import discord, ffmpeg
+from discord import app_commands
 from discord import FFmpegPCMAudio
 from dotenv import load_dotenv
 import os
@@ -7,41 +7,68 @@ import os
 
 
 load_dotenv()
-TOKEN = os.getenv("Token")
 
 
-bot = discord.Bot()
+intents = discord.Intents.default()
+Client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(Client)
 
-def is_connected(ctx):
-    return ctx.voice_client and ctx.voice_client.is_connected()
+@Client.event
+async def on_ready():
+    await tree.sync(guild=discord.Object(id=765412221130899466))
+    print("Ready!")
 
 
+def is_connected(interaction):
+    return interaction.user.voice
 
 # Ping command
-@bot.tree.command(description="Check bot latency")
-async def ping(ctx):
-    await ctx.respond(f"Latency is {bot.latency:.2f} seconds.")
-
-@bot.tree.command(description="Play the Future funk radio in VC")
-async def connect(ctx):
-    user = ctx.message.author
+@tree.command(name="ping", description="Check bot latency", guild=discord.Object(id=765412221130899466))
+async def ping(interaction):
+    await interaction.response.send_message(f"Latency is {Client.latency:.2f} seconds.")
     
-    if user.voice and user.voice.channel:
+
+@tree.command(name="connect", description="Play the Future funk radio in VC", guild=discord.Object(id=765412221130899466))
+async def connect(interaction):
+    bot_voice = interaction.guild.voice_client
+    user = interaction.user
+
+    if bot_voice:
+        # we're in a channel already
+            if bot_voice.channel.id == user.voice.channel.id:
+                # in the same voice channel as the user already
+                await interaction.response.send_message("Bot is already in your VC")
+                return
+
+        # move channels now - we're in VC but not the user's VC
+            await bot_voice.move_to(user.voice.channel)
+            await interaction.response.send_message("Moved to your channel")
+            return
+
+    
+    if interaction.user.voice:
+
         vc = await user.voice.channel.connect()
-        source = FFmpegPCMAudio("https://stream.zeno.fm/48533y95cnruv", before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+
+        source = discord.FFmpegPCMAudio(executable="ffmpeg-master-latest-win64-gpl-shared\\bin\\ffmpeg.exe", source="https://stream.zeno.fm/48533y95cnruv")
         vc.play(source)
-        await ctx.send(f"Now playing Future Funk Radio in {vc.channel.name}")
-    else:
-        await ctx.send("You need to be in a voice channel to use this command.")
+        await interaction.response.send_message(f"Now playing Future Funk Radio in {vc.channel.name}")
 
-@bot.tree.command(description="Disconnect from VC")
-async def Disconnect(ctx):
-    if is_connected(ctx):
-        await ctx.voice_client.disconnect()
-        await ctx.respond(f"Disconnected")
+        
     
+
     else:
-        await ctx.respond(f"You are not in a voice channel")
+        await interaction.response.send_message("You need to be in a voice channel to use this command.")
+
+@tree.command(name="disconnecttt", description="Disconnect from VC", guild=discord.Object(id=765412221130899466))
+async def Disconnect(interaction):
+    if is_connected(interaction):
+        bot_voice = interaction.guild.voice_client
+        await bot_voice.disconnect(force=False)
+        await interaction.response.send_message(f"Disconnected")
+
+    else:
+        await interaction.response.send_message(f"You are not in a voice channel")
 
 
-bot.run(TOKEN)
+Client.run(os.getenv("Token"))
